6995ce62603ec2275c4aaae6627f35733cf7c4e4
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-03-10 19:54:18 +0530

    add cardview to listitemview

--- app/src/main/java/com/example/andorid/goonj/Data.java
+++ app/src/main/java/com/example/andorid/goonj/Data.java
@@ -1,5 +1,7 @@
 package com.example.andorid.goonj;
 
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
 /**
  * Created by Sourabh on 25-Feb-17.
  */
@@ -9,12 +11,15 @@ public class Data {
     private String mEventDate;
     private String mEventSummary;
     private int mEventClass;
+    private int mColor;
 
     public Data(int mEventClass, String mEventName, String mEventDate,String mEventSummary) {
         this.mEventClass = mEventClass;
         this.mEventSummary = mEventSummary;
         this.mEventName = mEventName;
         this.mEventDate = mEventDate;
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        mColor = generator.getRandomColor();
     }
 
     public String getmEventName() {
@@ -31,5 +36,9 @@ public class Data {
 
     public int getmEventClass() {
         return mEventClass;
+    }
+
+    public int getmColor() {
+        return mColor;
     }
 }
