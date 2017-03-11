b39a40248b049d0d4d99a35dfc6c03b970cd3068
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-03-12 01:29:36 +0530

    Add cardview flip animation to all cardviews

--- app/src/main/java/com/example/andorid/goonj/MainActivity.java
+++ app/src/main/java/com/example/andorid/goonj/MainActivity.java
@@ -56,13 +56,6 @@ public class MainActivity extends AppCompatActivity
         findViewById(R.id.funimgv).setBackground(TextDrawable.builder().buildRect(String.valueOf("Fun".charAt(0)), ColorGenerator.MATERIAL.getRandomColor()));
         findViewById(R.id.sporimgv).setBackground(TextDrawable.builder().buildRect(String.valueOf("Sports".charAt(0)), ColorGenerator.MATERIAL.getRandomColor()));
         findViewById(R.id.fineimgv).setBackground(TextDrawable.builder().buildRect(String.valueOf("FA"), ColorGenerator.MATERIAL.getRandomColor()));
-        LinearLayout cultural = (LinearLayout) findViewById(R.id.cultural);
-        cultural.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadDataToList(2);
-            }
-        });
         final CardView cvtech = (CardView) findViewById(R.id.cvtechnical);
         final CardView cvtech2 = (CardView) findViewById(R.id.cvtechnical2);
         cvtech.setOnLongClickListener(new View.OnLongClickListener(){
@@ -124,6 +117,370 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        final CardView cvcult = (CardView) findViewById(R.id.cvcultural);
+        final CardView cvcult2 = (CardView) findViewById(R.id.cvcultural2);
+        cvcult.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvcult.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvcult2.setVisibility(View.VISIBLE);
+                                cvcult2.setRotationY(-90);
+                                cvcult.setVisibility(View.GONE);
+                                cvcult2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvcult2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvcult2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvcult.setVisibility(View.VISIBLE);
+                                cvcult.setRotationY(90);
+                                cvcult2.setVisibility(View.GONE);
+                                cvcult.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvcult.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                loadDataToList(2);
+            }
+        });
+        findViewById(R.id.backcult).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvcult2.callOnClick();
+            }
+        });
+
+
+        final CardView cvlite = (CardView) findViewById(R.id.cvliterature);
+        final CardView cvlite2 = (CardView) findViewById(R.id.cvliterature2);
+        cvlite.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvlite.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvlite2.setVisibility(View.VISIBLE);
+                                cvlite2.setRotationY(-90);
+                                cvlite.setVisibility(View.GONE);
+                                cvlite2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvlite2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvlite2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvlite.setVisibility(View.VISIBLE);
+                                cvlite.setRotationY(90);
+                                cvlite2.setVisibility(View.GONE);
+                                cvlite.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvlite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* loadDataToList(3); */
+            }
+        });
+        findViewById(R.id.backlite).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvlite2.callOnClick();
+            }
+        });
+
+        final CardView cvthea = (CardView) findViewById(R.id.cvtheatre);
+        final CardView cvthea2 = (CardView) findViewById(R.id.cvtheatre2);
+        cvthea.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvthea.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvthea2.setVisibility(View.VISIBLE);
+                                cvthea2.setRotationY(-90);
+                                cvthea.setVisibility(View.GONE);
+                                cvthea2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvthea2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvthea2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvthea.setVisibility(View.VISIBLE);
+                                cvthea.setRotationY(90);
+                                cvthea2.setVisibility(View.GONE);
+                                cvthea.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvthea.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* loadDataToList(4); */
+            }
+        });
+        findViewById(R.id.backthea).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvthea2.callOnClick();
+            }
+        });
+
+        final CardView cvfun = (CardView) findViewById(R.id.cvfun);
+        final CardView cvfun2 = (CardView) findViewById(R.id.cvfun2);
+        cvfun.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvfun.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvfun2.setVisibility(View.VISIBLE);
+                                cvfun2.setRotationY(-90);
+                                cvfun.setVisibility(View.GONE);
+                                cvfun2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvfun2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvfun2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvfun.setVisibility(View.VISIBLE);
+                                cvfun.setRotationY(90);
+                                cvfun2.setVisibility(View.GONE);
+                                cvfun.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvfun.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* loadDataToList(5); */
+            }
+        });
+        findViewById(R.id.backfun).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvfun2.callOnClick();
+            }
+        });
+        final CardView cvsports = (CardView) findViewById(R.id.cvsports);
+        final CardView cvsports2 = (CardView) findViewById(R.id.cvsports2);
+        cvsports.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvsports.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvsports2.setVisibility(View.VISIBLE);
+                                cvsports2.setRotationY(-90);
+                                cvsports.setVisibility(View.GONE);
+                                cvsports2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvsports2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvsports2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvsports.setVisibility(View.VISIBLE);
+                                cvsports.setRotationY(90);
+                                cvsports2.setVisibility(View.GONE);
+                                cvsports.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvsports.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* loadDataToList(6); */
+            }
+        });
+        findViewById(R.id.backsports).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvsports2.callOnClick();
+            }
+        });
+        final CardView cvfinearts = (CardView) findViewById(R.id.cvfinearts);
+        final CardView cvfinearts2 = (CardView) findViewById(R.id.cvfinearts2);
+        cvfinearts.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvfinearts.animate().rotationY(90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvfinearts2.setVisibility(View.VISIBLE);
+                                cvfinearts2.setRotationY(-90);
+                                cvfinearts.setVisibility(View.GONE);
+                                cvfinearts2.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+
+
+
+                    }
+
+                }, 0);
+                return true;
+            }
+        });
+
+        cvfinearts2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        cvfinearts2.animate().rotationY(-90).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                cvfinearts.setVisibility(View.VISIBLE);
+                                cvfinearts.setRotationY(90);
+                                cvfinearts2.setVisibility(View.GONE);
+                                cvfinearts.animate().rotationY(0).setDuration(200).setListener(null);
+                            }
+                        });
+                    }
+
+                }, 0);
+
+            }});
+
+        cvfinearts.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                /* loadDataToList(6); */
+            }
+        });
+        findViewById(R.id.backfinearts).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvfinearts2.callOnClick();
+            }
+        });
     }
 
             public void loadDataToList(final int eventClass) {
